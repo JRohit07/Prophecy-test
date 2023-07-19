4f0a84e938159fbe78360d75433e6aef5bfbb212
jrohit@prophecy.io <jrohit@prophecy.io>
2023-07-19 06:07:26 +0000

    ❇️ New     - Dataset        - test
    🔁 Modified
        - Pipeline
           - TestPipeline

---
+++ ScalaTest/DepedancyProject/pipelines/TestPipeline/code/src/main/scala/io/prophecy/pipelines/testpipeline/graph/test.scala
@@ -0,0 +1,22 @@
+package io.prophecy.pipelines.testpipeline.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.testpipeline.config.Context
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object test {
+
+  def apply(context: Context): DataFrame =
+    context.spark.read
+      .format("csv")
+      .option("header",                         true)
+      .option("sep",                            ",")
+      .schema(StructType(Array(StructField("a", StringType, true))))
+      .load("dbfs:/tmp/random/rohite2e")
+
+}
