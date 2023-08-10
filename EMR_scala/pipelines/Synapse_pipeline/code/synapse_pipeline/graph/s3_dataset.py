b4babfab0d61195d02d9e2869c5bd2a724aaf36c
jrohit@prophecy.io <jrohit@prophecy.io>
2023-08-10 12:56:24 +0000

    🔁 Modified     - Pipeline        - Synapse_pipeline     - Dataset        - s3_dataset

--- EMR_scala/pipelines/Synapse_pipeline/code/synapse_pipeline/graph/s3_dataset.py
+++ EMR_scala/pipelines/Synapse_pipeline/code/synapse_pipeline/graph/s3_dataset.py
@@ -8,6 +8,11 @@ from synapse_pipeline.udfs.UDFs import *
 
 def s3_dataset(spark: SparkSession) -> DataFrame:
     return spark.read\
+        .schema(
+          StructType([
+            StructField("order_id", StringType(), True), StructField("customer_id", StringType(), True), StructField("order_status", StringType(), True), StructField("order_category", StringType(), True), StructField("order_date", StringType(), True), StructField("amount", StringType(), True)
+        ])
+        )\
         .option("header", True)\
         .option("sep", ",")\
         .csv("s3://prophecy-dataset-samples/OrdersDatasetInput.csv")
