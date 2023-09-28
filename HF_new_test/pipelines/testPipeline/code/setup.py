8b7db8ac2bfb9780565635b07c9a52f97b04e1e0
jrohit@prophecy.io <jrohit@prophecy.io>
2023-09-28 12:13:54 +0000

    🔁 Modified     - Pipeline        - testPipeline_new

--- HF_new_test/pipelines/testPipeline/code/setup.py
+++ HF_new_test/pipelines/testPipeline/code/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'testPipeline',
+    name = 'testPipeline_new',
     version = '1.0',
     packages = find_packages(include = ('testpipeline*', )) + ['prophecy_config_instances'],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
