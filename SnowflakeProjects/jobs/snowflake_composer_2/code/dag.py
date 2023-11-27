3d0d97e1c316aec60ea1076f93f2e80deca931f1
jrohit@prophecy.io <jrohit@prophecy.io>
2023-11-27 09:58:39 +0000

    🔁 Modified     - Job        - snowflake_composer_2

--- SnowflakeProjects/jobs/snowflake_composer_2/code/dag.py
+++ SnowflakeProjects/jobs/snowflake_composer_2/code/dag.py
@@ -14,7 +14,8 @@ with DAG(
     dag_id = "staging_jrohit_Snowflake_project_external_git_snowflake_composer_2", 
     schedule_interval = "0 15/1 * * *", 
     default_args = {"owner" : "Prophecy", "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
-    start_date = pendulum.today('UTC'), 
-    catchup = True
+    start_date = pendulum.today('Asia/Calcutta'), 
+    catchup = True, 
+    tags = []
 ) as dag:
     SnowflakeSQL_0_op = SnowflakeSQL_0()
