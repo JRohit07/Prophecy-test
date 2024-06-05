6980948f6a45ee0617e7a581ce75dc5776887702
jrohit@prophecy.io <jrohit@prophecy.io>
2024-06-05 09:37:23 +0000

    Cloning prophecy project main branch

---
+++ TargetModelSql/pipelines/farmers-markets-irs/code/job/graph/farmers_market_tax_report.py
@@ -0,0 +1,14 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def farmers_market_tax_report(spark: SparkSession, in0: DataFrame):
+    in0.write\
+        .option("header", True)\
+        .option("sep", ",")\
+        .mode("overwrite")\
+        .option("separator", ",")\
+        .option("header", True)\
+        .csv("dbfs:/Prophecy/a95b0b9c2b76937c5ab701265a2a9cbf/farmers_market_report")
