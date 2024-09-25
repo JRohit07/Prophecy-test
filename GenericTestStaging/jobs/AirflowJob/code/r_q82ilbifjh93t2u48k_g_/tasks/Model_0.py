88293bf2450b07ab8ef12c26ee0c7101bb7d4d14
jrohit@prophecy.io <jrohit@prophecy.io>
2024-09-25 07:48:11 +0000

    Adds new tasks and updates configurations for Airflow and Databricks jobs in GenericTestStaging project

--- GenericTestStaging/jobs/AirflowJob/code/r_q82ilbifjh93t2u48k_g_/tasks/Model_0.py
+++ GenericTestStaging/jobs/AirflowJob/code/r_q82ilbifjh93t2u48k_g_/tasks/Model_0.py
@@ -21,17 +21,17 @@ def Model_0():
           "run_mode": "project",
           "entity_kind": None,
           "entity_name": None,
-          "project_id": None,
+          "project_id": "14304",
           "git_entity": "branch",
-          "git_entity_value": None,
-          "git_ssh_url": "",
-          "git_sub_path": "",
+          "git_entity_value": "devNewTests",
+          "git_ssh_url": "https://github.com/JRohit07/Prophecy-test",
+          "git_sub_path": "GenericTestStaging",
           "select": "",
           "threads": "",
           "exclude": "",
           "run_props": " --profile run_profile",
           "envs": {"DBT_DATABRICKS_INVOCATION_ENV" : "prophecy"}, 
-          "git_token_secret": "nidJQw_L3BkkAAQAYk9_iA_", 
-          "dbt_profile_secret": ""
+          "git_token_secret": "VKPYZO8SkMM7foBMzspE3w_", 
+          "dbt_profile_secret": "NBGW5z7eP5DyFVJyclt-j"
         },
     )
